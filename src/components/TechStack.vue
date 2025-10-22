946cdfd7fd361984bb3c8056cda2311e5dadb3b3
Voodoo30fps <voodoo30fps@gmail.com>
2025-10-22 16:35:37 +0300

    added tech stack icons, did the card design, added all the svgs

--- src/components/TechStack.vue
+++ src/components/TechStack.vue
@@ -1,30 +1,252 @@
+<script setup>
+import HTMLLogoIcon from './icons/HTMLLogoIcon.vue';
+import CSSLogoIcon from './icons/CSSLogoIcon.vue';
+import SASSLogoIcon from './icons/SASSLogoIcon.vue';
+import JavaSCriptLogoIcon from './icons/JavaSCriptLogoIcon.vue';
+import VuejsLogoIcon from './icons/VuejsLogoIcon.vue';
+import ReactLogoIcon from './icons/ReactLogoIcon.vue';
+import JQueryLogoIcon from './icons/JQueryLogoIcon.vue';
+import ReduxLogoIcon from './icons/ReduxLogoIcon.vue';
+import VuexLogoIcon from './icons/VuexLogoIcon.vue';
+import BootstrapLogoIcon from './icons/BootstrapLogoIcon.vue';
+import TailwindLogoIcon from './icons/TailwindLogoIcon.vue';
+import NPMLogoIcon from './icons/NPMLogoIcon.vue';
+import AJAXLogoIcon from './icons/AJAXLogoIcon.vue';
+import ResponsiveLogoIcon from './icons/ResponsiveLogoIcon.vue';
+import MobileFirstLogoIcon from './icons/MobileFirstLogoIcon.vue';
+import PHPLogoIcon from './icons/PHPLogoIcon.vue';
+import LaravelLogoIcon from './icons/LaravelLogoIcon.vue';
+import WordPressLogoIcon from './icons/WordPressLogoIcon.vue';
+import CMSLogoIcon from './icons/CMSLogoIcon.vue';
+import RESTAPILogoIcon from './icons/RESTAPILogoIcon.vue';
+import MySQLLogoIcon from './icons/MySQLLogoIcon.vue';
+import MongoDBLogoIcon from './icons/MongoDBLogoIcon.vue';
+import NGINXLogoIcon from './icons/NGINXLogoIcon.vue';
+import ApacheLogoIcon from './icons/ApacheLogoIcon.vue';
+import CPanelLogoIcon from './icons/CPanelLogoIcon.vue';
+import VercelLogoIcon from './icons/VercelLogoIcon.vue';
+import NetlifyLogoIcon from './icons/NetlifyLogoIcon.vue';
+import MicrosoftWindowsLogoIcon from './icons/MicrosoftWindowsLogoIcon.vue';
+import UbuntuLogoIcon from './icons/UbuntuLogoIcon.vue';
+import MacOSLogoIcon from './icons/MacOSLogoIcon.vue';
+import GithubMarkWhiteIcon from './icons/GithubMarkWhiteIcon.vue';
+import BitbucketLogoIcon from './icons/BitbucketLogoIcon.vue';
+import AzureDevOpsLogoIcon from './icons/AzureDevOpsLogoIcon.vue';
+import JiraLogoIcon from './icons/JiraLogoIcon.vue';
+import AsanaLogoIcon from './icons/AsanaLogoIcon.vue';
+import TrelloLogoIcon from './icons/TrelloLogoIcon.vue';
+import BrowserstackLogoIcon from './icons/BrowserstackLogoIcon.vue';
+import PostmanLogoIcon from './icons/PostmanLogoIcon.vue';
+import FirebaseLogoIcon from './icons/FirebaseLogoIcon.vue';
+import MongoDBCompassLogoIcon from './icons/MongoDBCompassLogoIcon.vue';
+</script>
+
 <template>
   <div class="tech-stack">
     <h2>Tech Stack 'n' Skills</h2>
 
-    <div>
-      Frontend: HTML & CSS, SCSS & SASS, JavaScript, Vue.js, React.js, jQuery,
-      state managementtools (Redux & Vuex), CSS Libraries (Bootstrap, Tailwind,
-      Skeleton), NPM, AJAX, responsive and mobile-first development.
+    <div class="cards-wrapper">
+      <div class="card">
+        <div class="card-heading">Frontend</div>
+        <div class="showcase">
+          <div>
+            <HTMLLogoIcon />
+            <span>HTML</span>
+          </div>
+          <div>
+            <CSSLogoIcon />
+            <span>CSS</span>
+          </div>
+          <div>
+            <SASSLogoIcon />
+            <span>SASS & SCSS</span>
+          </div>
+          <div>
+            <JavaSCriptLogoIcon />
+            <span>JavaScript</span>
+          </div>
+          <div>
+            <VuejsLogoIcon />
+            <span>Vue.js</span>
+          </div>
+          <div>
+            <ReactLogoIcon />
+            <span>React</span>
+          </div>
+          <div>
+            <JQueryLogoIcon />
+            <span>jQuery</span>
+          </div>
+          <div>
+            <ReduxLogoIcon />
+            <span>Redux</span>
+          </div>
+          <div>
+            <VuexLogoIcon />
+            <span>Vuex</span>
+          </div>
+          <div>
+            <BootstrapLogoIcon />
+            <span>Bootstrap</span>
+          </div>
+          <div>
+            <TailwindLogoIcon />
+            <span>Tailwind</span>
+          </div>
+          <div>
+            <NPMLogoIcon />
+            <span>NPM</span>
+          </div>
+          <div>
+            <AJAXLogoIcon />
+            <span>AJAX</span>
+          </div>
+          <div>
+            <ResponsiveLogoIcon />
+            <span>Responsive development</span>
+          </div>
+          <div>
+            <MobileFirstLogoIcon />
+            <span>Mobile-first development</span>
+          </div>
+        </div>
+      </div>
+
+      <div class="card">
+        <div class="card-heading">Backend</div>
+        <div class="showcase">
+          <div>
+            <PHPLogoIcon />
+            <span>PHP</span>
+          </div>
+          <div>
+            <LaravelLogoIcon />
+            <span>Laravel</span>
+          </div>
+          <div>
+            <WordPressLogoIcon />
+            <span>WordPress</span>
+          </div>
+          <div>
+            <CMSLogoIcon />
+            <span>CMS</span>
+          </div>
+          <div>
+            <RESTAPILogoIcon />
+            <span>REST API</span>
+          </div>
+          <div>
+            <MySQLLogoIcon />
+            <span>MySQL</span>
+          </div>
+          <div>
+            <MongoDBLogoIcon />
+            <span>MongoDB</span>
+          </div>
+          <div>
+            <NGINXLogoIcon />
+            <span>NGINX</span>
+          </div>
+          <div>
+            <ApacheLogoIcon />
+            <span>Apache</span>
+          </div>
+        </div>
+      </div>
+
+      <div class="card">
+        <div class="card-heading">Hosting tools</div>
+        <div class="showcase">
+          <div>
+            <CPanelLogoIcon />
+            <span>cPanel</span>
+          </div>
+          <div>
+            <VercelLogoIcon />
+            <span>Vercel</span>
+          </div>
+          <div>
+            <NetlifyLogoIcon />
+            <span>Netlify</span>
+          </div>
+        </div>
+      </div>
+
+      <div class="card">
+        <div class="card-heading">Operating Systems</div>
+        <div class="showcase">
+          <div>
+            <MicrosoftWindowsLogoIcon />
+            <span>Microsoft Windows</span>
+          </div>
+          <div>
+            <UbuntuLogoIcon />
+            <span>Linux (Ubuntu)</span>
+          </div>
+          <div>
+            <MacOSLogoIcon />
+            <span>macOS</span>
+          </div>
+        </div>
+      </div>
+
+      <div class="card">
+        <div class="card-heading">Version control</div>
+        <div class="showcase">
+          <div>
+            <GithubMarkWhiteIcon />
+            <span>Github</span>
+          </div>
+          <div>
+            <BitbucketLogoIcon />
+            <span>Bitbucket</span>
+          </div>
+        </div>
+      </div>
+
+      <div class="card">
+        <div class="card-heading">Task Management & Agile methodology</div>
+        <div class="showcase">
+          <div>
+            <AzureDevOpsLogoIcon />
+            <span>Azure DevOps</span>
+          </div>
+          <div>
+            <JiraLogoIcon />
+            <span>Jira</span>
+          </div>
+          <div>
+            <AsanaLogoIcon />
+            <span>Asana</span>
+          </div>
+          <div>
+            <TrelloLogoIcon />
+            <span>Trello</span>
+          </div>
+        </div>
+      </div>
+
+      <div class="card">
+        <div class="card-heading">Other tools</div>
+        <div class="showcase">
+          <div>
+            <BrowserstackLogoIcon />
+            <span>Browserstack</span>
+          </div>
+          <div>
+            <PostmanLogoIcon />
+            <span>Postman</span>
+          </div>
+          <div>
+            <FirebaseLogoIcon />
+            <span>Firebase</span>
+          </div>
+          <div>
+            <MongoDBCompassLogoIcon />
+            <span>Mongo DB Compass</span>
+          </div>
+        </div>
+      </div>
     </div>
-
-    <div>
-      Backend: PHP, Laravel, WordPress, CMS, REST API, MySQL, MongoDB, NGINX,
-      Apache.
-    </div>
-
-    <div>Hosting tools: cPanel, Vercel, Netlify.</div>
-
-    <div>
-      Operating Systems: Microsoft Windows, Linux (Ubuntu), Apple iOS (support &
-      troubleshooting)
-    </div>
-
-    <div>
-      Version control: Github, Bitbucket.Agile methodology: Azure DevOps, Jira,
-      Asana, Trello.
-    </div>
-
-    <div>Other tools: Browserstack, Postman, Firebase, Mongo DB Compass.</div>
   </div>
 </template>
