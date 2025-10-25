9d97e88cf73c385dfa56dfeb854852732738cee1
Voodoo30fps <voodoo30fps@gmail.com>
2025-10-25 21:42:44 +0300

    footer

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@ import About from './components/About.vue';
 import TechStack from './components/TechStack.vue';
 import Experience from './components/Experience.vue';
 import ProjectsAndCourses from './components/ProjectsAndCourses.vue';
+import Footer from './components/Footer.vue';
 </script>
 
 <template>
@@ -12,4 +13,6 @@ import ProjectsAndCourses from './components/ProjectsAndCourses.vue';
   <TechStack />
   <Experience />
   <ProjectsAndCourses />
+
+  <Footer />
 </template>
---
+++ src/components/Footer.vue
@@ -0,0 +1,9 @@
+<template>
+  <footer>
+    <h4>Let's build something together.</h4>
+    <p>
+      Whether you have a project in mind or just want to say hi, I’m always
+      happy to chat.
+    </p>
+  </footer>
+</template>
